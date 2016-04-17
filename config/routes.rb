484c6d87d2fa61e5bf494e098020c65e5ba773fb
umutbarisisler <umutbaisisler@gmail.com>
2016-04-17 03:06:04 +0300

    finish about & contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#home'
 
+  get 'contact' => 'static_pages#contact'
+
+  get 'about' => 'static_pages#about'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
